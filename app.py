9cb51d3a7c00609e97c6f141cfc8c9865d02d15e
ZPXD <zajecia-programowania-xd@gmail.com>
2022-03-30 17:53:36 +0200

    dodany przycisk

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
 import random
+import pandas as pd
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def index():
-    text = open('dane/sample.txt').readlines()
-    text = random.choice(text)
-    return render_template("index.html", text=text)
+
+    # Dane
+    df = pd.read_csv('data/sample.csv')
+    
+    # Losowanie pytania
+    i = random.choice(range(len(df)))
+    this_question = df.loc[i, :]
+    question = this_question['question']
+    answer = this_question['answer']
+
+    return render_template("index.html", question=question, answer=answer)
 
 if __name__=="__main__":
     app.run()
